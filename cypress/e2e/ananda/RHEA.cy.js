79ebf8232c799579b1087ca44aea601defc605dc
amallipudi <amallipudi@targray.com>
2023-11-01 13:14:21 +0530

    fff

--- cypress/e2e/ananda/RHEA.cy.js
+++ cypress/e2e/ananda/RHEA.cy.js
@@ -13,15 +13,31 @@ describe('RHEA Application', () => {
         
       })
     it('verifies the title', () => { 
+      cy.visit('https://targray-corp-uat.crm3.dynamics.com/main.aspx?appid=5a338062-8977-ed11-81ac-0022486df36c&pagetype=entitylist&etn=tar_strip&viewid=65e28876-255c-ed11-9562-000d3af4fee0&viewType=1039').wait(10000)
+  
+      cy.origin('https://targray-corp-uat.crm3.dynamics.com', () => { 
+        cy.get('#okButtonText_1').click()
+        cy.get('[aria-rowindex="4"] > [aria-colindex="2"] > .ms-Shimmer-container > .ms-Shimmer-dataWrapper > .containerEditableStyles-273 > .ms-Stack > .truncatableText-290 > .ms-Label > .ms-TooltipHost').dblclick()
       
-   
     })
+  })
+
     it('verifies the name', () => { 
-      
-   
+      cy.visit('https://targray-corp-uat.crm3.dynamics.com/main.aspx?appid=5a338062-8977-ed11-81ac-0022486df36c&pagetype=entitylist&etn=tar_strip&viewid=65e28876-255c-ed11-9562-000d3af4fee0&viewType=1039').wait(10000)
+  
+      cy.origin('https://targray-corp-uat.crm3.dynamics.com', () => { 
+        cy.get('#okButtonText_1').click()
+      cy.get('#id__7').click()
+    })
     })
     it('verifies the user ', () => { 
-      
-   
-    })
-            })
+      cy.visit('https://targray-corp-uat.crm3.dynamics.com/main.aspx?appid=5a338062-8977-ed11-81ac-0022486df36c&pagetype=entitylist&etn=tar_strip&viewid=65e28876-255c-ed11-9562-000d3af4fee0&viewType=1039').wait(10000)
+     
+      cy.origin('https://targray-corp-uat.crm3.dynamics.com', () => { 
+        cy.get('#okButtonText_1').click()
+        cy.get('#quickFind_text_0').type('TCTS-1003{enter}')
+  
+})
+             
+        })
+      })
