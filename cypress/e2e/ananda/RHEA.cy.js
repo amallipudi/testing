e80dc05115e467d6e6059790fc55d9ab30cddb54
amallipudi <amallipudi@targray.com>
2023-11-01 16:42:38 +0530

    www

--- cypress/e2e/ananda/RHEA.cy.js
+++ cypress/e2e/ananda/RHEA.cy.js
@@ -3,7 +3,7 @@ describe('RHEA Application', () => {
   beforeEach(() => {
     cy.loginToAAD(Cypress.env('aad_username'), Cypress.env('aad_password'))
       
-    cy.visit('https://targray-corp-uat.crm3.dynamics.com/main.aspx?appid=5a338062-8977-ed11-81ac-0022486df36c&pagetype=entitylist&etn=tar_strip&viewid=65e28876-255c-ed11-9562-000d3af4fee0&viewType=1039').wait(10000)
+    cy.visit('https://targray-corp-uat.crm3.dynamics.com/main.aspx?appid=5a338062-8977-ed11-81ac-0022486df36c&pagetype=entitylist&etn=tar_strip&viewid=65e28876-255c-ed11-9562-000d3af4fee0&viewType=1039')
     cy.viewport(1800, 1400)
         Cypress.on('uncaught:exception', (err, runnable) => {
             // returning false here prevents Cypress from
@@ -11,11 +11,9 @@ describe('RHEA Application', () => {
             return false
         })
         
-      })
+      
     it('verifies the title', () => { 
-      cy.visit('https://targray-corp-uat.crm3.dynamics.com/main.aspx?appid=5a338062-8977-ed11-81ac-0022486df36c&pagetype=entitylist&etn=tar_strip&viewid=65e28876-255c-ed11-9562-000d3af4fee0&viewType=1039').wait(10000)
-  
-      cy.origin('https://targray-corp-uat.crm3.dynamics.com', () => { 
+      cy.get('#okButtonText_1').click()
          
         cy.get('[aria-rowindex="4"] > [aria-colindex="2"] > .ms-Shimmer-container > .ms-Shimmer-dataWrapper > .containerEditableStyles-273 > .ms-Stack > .truncatableText-290 > .ms-Label > .ms-TooltipHost').dblclick()
       
@@ -23,21 +21,15 @@ describe('RHEA Application', () => {
   })
 
     it('verifies the name', () => { 
-      cy.visit('https://targray-corp-uat.crm3.dynamics.com/main.aspx?appid=5a338062-8977-ed11-81ac-0022486df36c&pagetype=entitylist&etn=tar_strip&viewid=65e28876-255c-ed11-9562-000d3af4fee0&viewType=1039').wait(10000)
-  
-      cy.origin('https://targray-corp-uat.crm3.dynamics.com', () => { 
-        
+      cy.get('#okButtonText_1').click()
       cy.get('#id__7').click()
     })
-    })
+    
     it('verifies the user ', () => { 
-      cy.visit('https://targray-corp-uat.crm3.dynamics.com/main.aspx?appid=5a338062-8977-ed11-81ac-0022486df36c&pagetype=entitylist&etn=tar_strip&viewid=65e28876-255c-ed11-9562-000d3af4fee0&viewType=1039').wait(10000)
-     
-      cy.origin('https://targray-corp-uat.crm3.dynamics.com', () => { 
+       
         cy.get('#okButtonText_1').click()
         cy.get('#quickFind_text_0').type('TCTS-1003{enter}')
   
 })
              
-        })
-      })
+}) 
