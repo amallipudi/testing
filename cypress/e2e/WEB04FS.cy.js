8f21b8e621a7786fa1f6c0d75e18bbd4da42c940
amallipudi <amallipudi@targray.com>
2023-11-02 13:33:41 +0530

    eeee

---
+++ cypress/e2e/WEB04FS.cy.js
@@ -0,0 +1,15 @@
+describe('FEEDSTOCK CONTRACT Application', () => {
+    beforeEach(() => {
+      cy.ntlm(["web04"], Cypress.env("username") , Cypress.env("password"));
+      cy.visit("http://web04/TEST-TFSContractManagement/Pages/Deals")
+      cy.viewport(1800, 1400)
+    })
+  
+it('FEEDSTOCK  ', (done) =>{
+    cy.get('img').should("be.visible")
+    cy.url().should ('include','/Pages/Deals')
+    .then(() => {
+        done()    
+}) 
+})
+})
--- cypress/e2e/web04.cy.js
+++ cypress/e2e/web04.cy.js
@@ -1,25 +1,16 @@
 
-    describe('PULSES CONTRACT Application', () => {
+    describe('FEEDSTOCK CONTRACT Application', () => {
         beforeEach(() => {
           cy.ntlm(["web04"], Cypress.env("username") , Cypress.env("password"));
           cy.visit("http://web04/TEST-TPSContractManagement/Pages/Deals")
+          cy.viewport(1800, 1400)
         })
-      it('PULSES CONTRACT Application', () => {
-        
-        cy.get('#lnkbtnAdd').click(); // ADD NEW DEAL
-        cy.get('.p-l-5 > .btn-group > .btn > .filter-option').click(); // Great Plains Company
-        cy.get('.p-l-5 > .btn-group > .open > .dropdown-menu > [data-original-index="2"] > a').click();
-    
-        cy.get('#cphContent_rblDealType > tbody > tr > :nth-child(1) > label').click(); // deal type
-it('Check weather Enable Single or multiple Document Upload feature in BIO Europe Logistics App. ', (done) =>{
+      
+it('FEEDSTOCK  ', (done) =>{
         cy.get('img').should("be.visible")
-        cy.get('#lnkbtnAdd').click()
-        //cy.get('.confirm').click()
-        
-})
-it('empty ', (done) =>{
-     
-    
-})
-})
+        cy.url().should ('include','/Pages/Deals')
+        .then(() => {
+            done()    
+}) 
     })
+  })
