540026645ec81409501eac541d26bd12a49309a9
amallipudi <amallipudi@targray.com>
2023-11-02 17:43:55 +0530

    ttt

--- cypress/e2e/WEB04FS.cy.js
+++ cypress/e2e/WEB04FS.cy.js
@@ -8,7 +8,7 @@ describe('FEEDSTOCK CONTRACT Application', () => {
 it('FEEDSTOCK  ', (done) =>{
     cy.get('img').should("be.visible")
     cy.get(':nth-child(1) > :nth-child(16) > #lnkbtnEditDetails > .far').click()
-    cy.get('#txtBxDealNotes').clear().type('Azure Pipelines hosted pool with various Windows, Linux, and macOS images. For a complete list of the available images and their installed software, see Microsoft-hosted agents.')
+    cy.get('#txtBxDealNotes').clear().type('Default pool: Use it to register self-hosted agents that you')
     cy.get('#lnkUpdateDeal').click()
     cy.get('.breadcrumb > :nth-child(1) > a').click()
     cy.url().should ('include','/Pages/Deals')
